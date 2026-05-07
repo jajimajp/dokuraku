6c102e39b88279e8229a4f8d8325bfb220f27f73
Yajima <119467077+jajimajp@users.noreply.github.com>
2026-05-07 23:42:49 +0900

    Implement < for dokuraku

--- dokuraku.lisp
+++ dokuraku.lisp
@@ -118,6 +118,7 @@
      (cond ((= '+ (car v)) (sum (cdr (eval-list-elems v))))
            ((= '* (car v)) (multiply (cdr (eval-list-elems v))))
            ((= '= (car v)) (= (eval (cadr v)) (eval (caddr v))))
+           ((= '< (car v)) (< (eval (cadr v)) (eval (caddr v))))
            (t nil)))
     (t nil)))
 
