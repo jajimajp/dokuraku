d4dfdccc4cf9dfcff0da82de3b6d0ba9b9da07c1
Yajima <119467077+jajimajp@users.noreply.github.com>
2026-05-08 23:54:54 +0900

    Add lambda

--- test_dokuraku.rb
+++ test_dokuraku.rb
@@ -55,3 +55,4 @@ assert_value '#\\a', '#\\a' # escape backslash
 assert_value '"str"', '"str"'
 assert_value '1', '(progn (defun f () 1) (f))'
 assert '5', '(defparameter v 5) (write v)'
+assert '4', '(defparameter double (lambda (x) (* x 2))) (write (double 2))'
